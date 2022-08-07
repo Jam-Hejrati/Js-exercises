b1f7be943243d8f9fe67b80e834dd7d81e9bd296
jam <jamhejratipro@gmail.com>
2022-08-08 00:00:40 +0430

    string methods

--- Fundementals/script.js
+++ Fundementals/script.js
@@ -39,14 +39,64 @@ const restaurant = {
     },
 };
 
-const setToMap = (map , key , value) => map.set(key , value)
-const getFromMap = (map , key) => map.get(key)
+const airLine = "TAP Air Portugal";
 
-const rest = new Map([
-    [1 , 'this is the first value'],
-    ['yoo ha ha ha' , true],
-    [false , 45],
-    ['test' , 'ok'] 
-])
-console.log(getFromMap(rest , 1));
-console.log(rest);
+console.log(airLine.indexOf('r'));
+console.log(airLine.lastIndexOf('r'));
+console.log(airLine.indexOf('Air'));
+console.log(airLine.slice(6));
+console.log(airLine.slice(6 , 11)); // not contains the character at index 11
+console.log(airLine[11]); // see? didn't contain
+console.log(airLine.slice(airLine.indexOf('Air')));
+
+/* slice the FIRST word of string without knowing the length and... */
+console.log(airLine.slice(0, airLine.indexOf(" ")));
+
+/* slice the LAST word of string without knowing the length and... */
+console.log(airLine.slice(airLine.lastIndexOf(" ") + 1));
+
+console.log(airLine.slice(-3));
+
+const email = '   Hello@Jam.Io \n'
+console.log(email);
+console.log(email.trim().toLocaleLowerCase()); //normalized email
+
+const ennouncement =
+    "All passengers come to boarding door 23, Boarding door 23!";
+console.log("replace(): ", ennouncement.replace("door", "gate"));
+console.log("replaceAll(): ", ennouncement.replaceAll("door", "gate"));
+console.log("replace() with RegEx: ", ennouncement.replace(/door/g, "gate")); //RegEx ~~> Regular Expressions
+
+const plane = 'A320neo'
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+console.log(plane.startsWith('A3'));
+console.log(plane.endsWith('eo'));
+
+const [firstName , lastName] = 'Jam Hejrati'.split(' ')
+console.log(firstName);
+console.log(lastName);
+const fullName = ['Mr.' , firstName , lastName].join(' ')
+console.log(fullName);
+
+const capitalize = (name) => {
+    const nameArr = name.split(" ");
+    const fullCapitalizeName = [];
+    for (const element of nameArr) {
+        // fullCapitalizeName.push(element[0].toUpperCase() + element.slice(1))
+        fullCapitalizeName.push(
+            element.replace(element[0], element[0].toUpperCase())
+        ); //another way
+    }
+    return fullCapitalizeName.join(" ");
+};
+console.log(capitalize("jam hejrati"));
+
+const message = 'Go to gate 23!'
+console.log(message.padStart(25 , '-'));
+console.log(message.padEnd(25 , '-'));
+
+const message2 = 'Bad weather... All Departues Deployed...\n'
+console.log(message2.repeat(5));
+
