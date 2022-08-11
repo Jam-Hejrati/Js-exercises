31369374d1de1bf749e3bc3224b47be7697de8a4
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-11 17:41:35 +0430

    Call and Apply

--- Fundementals/script.js
+++ Fundementals/script.js
@@ -1,18 +1,46 @@
-'use strict';
+'use strict'
 
-const greet = function (greeting) {
-    return function (name) {
-        console.log(`${greeting} ${name}`)
-    }
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    booking: [],
+
+    book(flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`)
+        this.booking.push({flight: `${this.iataCode}${flightNum}`, name})
+    },
 }
 
-const greeterHey = greet('Hey')
-greeterHey('Jonas')
-greeterHey('Jam')
-const greeterWelcome = greet('Welcome')
-greeterWelcome('Ali ')
-/* Or ~~> */
-greet('Welcome')('Ali')
 
-const greetArrow = greeting => name => console.log(`${greeting} ${name}`)
-greetArrow('Hello')('Jam')
+const eurowing = {
+    airline: 'Eurowing',
+    iataCode: 'EW',
+    booking: [],
+
+}
+
+const book = lufthansa.book;
+
+//wrong and NOT working
+// book(23 , 'Sara Williams')
+
+book.call(eurowing , 23 , 'Sara Williams')
+
+book.call(lufthansa , 239 , 'Merry Cooper')
+
+const swiss = {
+    airline: 'Swiss Air Lines',
+    iataCode: 'LX',
+    booking:[],
+}
+
+book.call(swiss , 583 , 'Ali Sediqi')
+
+const flightData = [486 , 'Jam Hejrati'];
+book.apply(swiss , flightData)
+/* {Call} methods first arg is the object for (this) keywords refer and then get ,
+*  the function arguments BUT {Apply} method is exactly like {Call} but instead of functions
+*  arguments , it gets an array of args */
+
+/* In modern Js we do not use apply method anymore because we can use spread operator in {Call} method */
+book.call(swiss  , ...flightData)
