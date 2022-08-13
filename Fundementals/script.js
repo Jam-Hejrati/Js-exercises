37e944216a8e0bdb766abc2160d4e9417aeb7883
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-13 21:54:25 +0430

    closure exp

--- Fundementals/script.js
+++ Fundementals/script.js
@@ -1,15 +1,41 @@
 'use strict'
 
-const secureBooking = function(){
-    let passengerCount = 0
+// Example 1
+let f;
 
-    return function() {
-        passengerCount++
-        console.log(`${passengerCount} passengers`)
+const g = function () {
+    const a = 23
+    f = function () {
+        console.log(a * 2)
     }
 }
 
-const booker = secureBooking();
-booker() // passengerCount = 1
-booker() // passengerCount = 2
-booker() // passengerCount = 3
+const h = function () {
+    const b = 777
+    f = function () {
+        console.log(b * 2)
+    }
+}
+
+g()
+f()
+
+//RE-assigning f function
+h()
+f()
+
+
+// Example 2
+const boardPassengers = function (n , wait) {
+    const perGroup = n / 3
+
+    setTimeout(function () {
+        console.log(`We are now boarding all ${n} passengers`)
+        console.log(`there are 3 groups , each with ${perGroup} passengers`)
+    } , wait * 1000)
+
+    console.log(`Will start boarding in ${wait} seconds`)
+}
+
+const perGroup = 1000
+boardPassengers(180 , 3)
