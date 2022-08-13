1839a0140cc0e1685ac9ee279f5f73f7df6c4cbb
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-13 20:49:24 +0430

    IIFE

--- Fundementals/script.js
+++ Fundementals/script.js
@@ -1,29 +1,10 @@
-'use strict'
+'use strict';
 
-const poll = {
-    question: 'What is your favourite programming language?',
-    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-    answers: new Array(4).fill(0),
-    registerNewAnswer() {
-        const answer = Number(
-            prompt(
-                `${this.question}\n${this.options.join('\n')}\n(Write option number)`
-            )
-        );
 
-        typeof answer === 'number' &&
-        answer < this.answers.length &&
-        this.answers[answer]++;
+//IIFE
+(function () {
+    console.log("this will never run again")
+    const isPrivate = 23
+})();
 
-        this.displayResults();
-        this.displayResults('string');
-    },
-    displayResults(type = 'array') {
-        type === 'array' && console.log(this.answers)
-        type === 'string' && console.log(`Poll results are ${this.answers.join(', ')}`);
-
-    },
-};
-document
-    .querySelector('.poll')
-    .addEventListener('click', poll.registerNewAnswer.bind(poll));
+(() => console.log('this will ALSO never run again'))()
