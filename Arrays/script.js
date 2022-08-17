0a2c71f5579d5e03f74ea9742caf5a020f3f7674
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-17 13:49:11 +0430

    reduce method

--- Arrays/script.js
+++ Arrays/script.js
@@ -108,10 +108,12 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 */
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-const deposits = movements.filter((mov) => mov > 0);
-const withdrawals = movements.filter((mov) => mov < 0);
-
 console.log(movements);
-console.log(deposits);
-console.log(withdrawals);
+
+//accumulator >=> snowball
+const balance = movements.reduce(
+  (accumulator, current, index, arr) => accumulator + current,
+  100 //this second argument of reduce() is the initial value of accumulator
+); 
+
+console.log(balance);
