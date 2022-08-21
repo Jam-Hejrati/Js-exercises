26554e1f4fc598aded0c0c6c98e58db8b7d4bcbd
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-21 12:30:34 +0430

    flat() & flatMap()

--- Arrays/script.js
+++ Arrays/script.js
@@ -6,31 +6,31 @@
 
 // Data
 const account1 = {
-    owner: "Jonas Schmedtmann",
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    interestRate: 1.2, // %
-    pin: 1111,
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
 };
 
 const account2 = {
-    owner: "Jessica Davis",
-    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-    interestRate: 1.5,
-    pin: 2222,
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
 };
 
 const account3 = {
-    owner: "Steven Thomas Williams",
-    movements: [200, -200, 340, -300, -20, 50, 400, -460],
-    interestRate: 0.7,
-    pin: 3333,
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
 };
 
 const account4 = {
-    owner: "Sarah Smith",
-    movements: [430, 1000, 700, 50, 90],
-    interestRate: 1,
-    pin: 4444,
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -63,147 +63,158 @@ const inputClosePin = document.querySelector(".form__input--pin");
 
 // set data visually in the movement box
 const displayMovements = function (movements) {
-    containerMovements.innerHTML = "";
-    movements.forEach(function (mov, index) {
-        const type = mov > 0 ? "deposit" : "withdrawal";
-        const html = `
+  containerMovements.innerHTML = "";
+  movements.forEach(function (mov, index) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const html = `
             <div class="movements__row">
                   <div class="movements__type movements__type--${type}">${
-            index + 1
-        } ${type}</div>
+      index + 1
+    } ${type}</div>
                   <div class="movements__value">${mov}€</div>
             </div>
             `;
 
-        containerMovements.insertAdjacentHTML("afterbegin", html);
-    });
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
 };
 
 // set the sum of movements visually on Top
 const calcDisplayBalance = (acc) => {
-    acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-    labelBalance.textContent = `${acc.balance}€`;
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
 };
 
 // set the Sum of deposit, withdrawal & interest values visually
 const calcDisplaySummary = (acc) => {
-    const incomes = acc.movements
-        .filter((mov) => mov > 0)
-        .reduce((acc, mov) => acc + mov, 0);
-    labelSumIn.textContent = `${incomes}€`;
+  const incomes = acc.movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
 
-    const out = acc.movements
-        .filter((mov) => mov < 0)
-        .reduce((acc, mov) => acc + mov, 0);
-    labelSumOut.textContent = `${Math.abs(out)}€`;
+  const out = acc.movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
 
-    const interest = acc.movements
-        .filter((mov) => mov > 0)
-        .map((deposit) => (deposit * acc.interestRate) / 100)
-        .filter((interest, i, arr) => interest >= 1)
-        .reduce((acc, interest) => acc + interest, 0);
-    labelSumInterest.textContent = `${interest}€`;
+  const interest = acc.movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * acc.interestRate) / 100)
+    .filter((interest, i, arr) => interest >= 1)
+    .reduce((acc, interest) => acc + interest, 0);
+  labelSumInterest.textContent = `${interest}€`;
 };
 
 // make abbreviation of owners Full name (username)
 const createUsernames = (accs) => {
-    accs.forEach(
-        (acc) =>
-            (acc.username = acc.owner
-                .toLowerCase()
-                .split(" ")
-                .map((word) => word[0])
-                .join(""))
-    );
+  accs.forEach(
+    (acc) =>
+      (acc.username = acc.owner
+        .toLowerCase()
+        .split(" ")
+        .map((word) => word[0])
+        .join(""))
+  );
 };
 createUsernames(accounts);
 
 const updateUI = function (acc) {
-    // Display movements
-    displayMovements(acc.movements);
+  // Display movements
+  displayMovements(acc.movements);
 
-    // Display balance
-    calcDisplayBalance(acc);
+  // Display balance
+  calcDisplayBalance(acc);
 
-    // Display summary
-    calcDisplaySummary(acc);
+  // Display summary
+  calcDisplaySummary(acc);
 }; // Just to make it globally
 
 // Event handler
 let currentAccount;
 btnLogin.addEventListener("click", (e) => {
-    //Prevent form from submitting
-    e.preventDefault();
+  //Prevent form from submitting
+  e.preventDefault();
 
-    currentAccount = accounts.find(
-        (acc) => acc.username === inputLoginUsername.value
-    );
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
 
-    if (currentAccount?.pin === +inputLoginPin.value) {
-        // Display UI and message
-        labelWelcome.textContent = `Welcome back, ${
-            currentAccount.owner.split(" ")[0]
-        }`;
-        containerApp.style.opacity = 100;
+  if (currentAccount?.pin === +inputLoginPin.value) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    containerApp.style.opacity = 100;
 
-        // Clear input Fields
-        inputLoginUsername.value = inputLoginPin.value = ''
-        inputLoginPin.blur(); // lose the cursors :focus
+    // Clear input Fields
+    inputLoginUsername.value = inputLoginPin.value = "";
+    inputLoginPin.blur(); // lose the cursors :focus
 
-        // Update UI
-        updateUI(currentAccount)
-    }
+    // Update UI
+    updateUI(currentAccount);
+  }
 });
 
-btnTransfer.addEventListener('click', e => {
-    e.preventDefault();
-    const amount = +inputTransferAmount.value
-    const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value)
-    inputTransferAmount.value = inputTransferTo.value = ''
+btnTransfer.addEventListener("click", (e) => {
+  e.preventDefault();
+  const amount = +inputTransferAmount.value;
+  const receiverAcc = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = "";
 
-    if (amount > 0 &&
-        receiverAcc &&
-        currentAccount.balance >= amount &&
-        receiverAcc?.username !== currentAccount.username
-    ) {
-        // Doing the transfer
-        currentAccount.movements.push(-amount)
-        receiverAcc.movements.push(amount)
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
 
-        // Update UI
-        updateUI(currentAccount)
-    }
-})
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
 
-btnLoan.addEventListener('click', e => {
-    e.preventDefault();
+btnLoan.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    const amount = +inputLoanAmount.value
-    if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-        // Add movement
-        currentAccount.movements.push(amount)
+  const amount = +inputLoanAmount.value;
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    // Add movement
+    currentAccount.movements.push(amount);
 
-        // Update UI
-        updateUI(currentAccount)
-    }
+    // Update UI
+    updateUI(currentAccount);
+  }
 
-    inputLoanAmount.value = ''
-})
+  inputLoanAmount.value = "";
+});
 
-btnClose.addEventListener('click', e => {
-    e.preventDefault();
+btnClose.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    if (inputCloseUsername.value === currentAccount.username && +inputClosePin.value === currentAccount.pin) {
-        const index = accounts.findIndex(acc => acc.username === currentAccount.username)
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    +inputClosePin.value === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
 
-        // Delete account
-        accounts.splice(index, 1)
+    // Delete account
+    accounts.splice(index, 1);
 
-        // hide UI
-        containerApp.style.opacity = 0
-    }
-    inputCloseUsername.value = inputClosePin.value = ''
-})
+    // hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = "";
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -219,12 +230,23 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-// every() method will return true if every element in array satisfies the condition
-console.log(movements.every(mov => mov > 0)) // false
-console.log(movements.every(mov => Math.abs(mov) > 50))  // true
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat()); // [1, 2, 3, 4, 5, 6, 7, 8]
+// Flat() will go just ONE level deep
 
-// Separate callback
-const deposit = mov => mov > 0
-console.log(movements.some(deposit))
-console.log(movements.every(deposit))
-console.log(movements.filter(deposit))
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat()); // [[1, 2], 3, 4, [5, 6], 7, 8]
+console.log(arrDeep.flat(2)); // [1, 2, 3, 4, 5, 6, 7, 8]
+
+// flat()
+const overalBalance = accounts
+  .map((acc) => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+//flatMap() ~~> flat() + map() >==> it will go just ONE level deep and we CAN'T change it
+const overalBalance2 = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
