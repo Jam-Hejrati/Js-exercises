799ef557d8db4b9b5db1b346c494e3310000939a
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-21 12:48:28 +0430

    sort()

--- Arrays/script.js
+++ Arrays/script.js
@@ -230,23 +230,29 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
-console.log(arr.flat()); // [1, 2, 3, 4, 5, 6, 7, 8]
-// Flat() will go just ONE level deep
+const owners = ["Jonas", "Zach", "Adam", "Martha"];
+console.log(owners.sort());
+console.log(owners);
 
-const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
-console.log(arrDeep.flat()); // [[1, 2], 3, 4, [5, 6], 7, 8]
-console.log(arrDeep.flat(2)); // [1, 2, 3, 4, 5, 6, 7, 8]
+/* For numbers If a callback func in sort() method returns: */
+// return < 0 => A ,B (keep order)
+// return > 0 => B ,A (switch order)
+console.log(movements);
+/*movements.sort((a,b)=> {
+    if(a > b) return 1
+    if( b > a) return -1
+})*/
 
-// flat()
-const overalBalance = accounts
-  .map((acc) => acc.movements)
-  .flat()
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(overalBalance);
+// Ascending
+movements.sort((a, b) => (a > b ? 1 : -1));
+console.log(movements);
+// Descending
+movements.sort((a, b) => (a < b ? 1 : -1));
+console.log(movements);
 
-//flatMap() ~~> flat() + map() >==> it will go just ONE level deep and we CAN'T change it
-const overalBalance2 = accounts
-  .flatMap((acc) => acc.movements)
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(overalBalance2);
+// Ascending
+movements.sort((a, b) => a - b);
+console.log(movements);
+// Descending
+movements.sort((a, b) => b - a);
+console.log(movements);
