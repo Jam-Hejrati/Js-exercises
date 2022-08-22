94e03b1a6f5f02fb04dbdc88655566a2f2add197
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-22 12:14:22 +0430

    Data changed

--- Arrays/script.js
+++ Arrays/script.js
@@ -6,34 +6,46 @@
 
 // Data
 const account1 = {
-  owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
-  owner: "Jessica Davis",
+  owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector(".welcome");
@@ -240,36 +252,3 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-const arr = [1, 2, 3, 4, 5, 6, 7];
-const x = new Array(7); // Length will be 7 with 7 empty element
-console.log(x);
-
-x.fill(1, 3, 5); // .fill(element , start , end)
-x.fill(1); //[1, 1, 1, 1, 1, 1, 1]
-console.log(x);
-
-arr.fill(23, 4, 6); // [1, 2, 3, 4, 23, 23, 7]
-console.log(arr);
-
-// Array.from
-const y = Array.from({ length: 7 }, () => 1);
-console.log(y);
-
-const z = Array.from({ length: 7 }, (current, index) => index + 1);
-console.log(z);
-
-// An array of random dice rolls with length = 10
-const randomDiceRolls = Array.from(
-  { length: 10 },
-  () => Math.trunc(Math.random() * 6) + 1
-);
-console.log(randomDiceRolls);
-
-labelBalance.addEventListener("click", () => {
-  const movementsUI = Array.from(
-    document.querySelectorAll(".movements__value"),
-    (el) => +el.textContent.replace("€", "")
-  );
-
-  console.log(movementsUI);
-});
