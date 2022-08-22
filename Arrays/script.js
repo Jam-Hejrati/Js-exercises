89e801828429b4d8724d8e20135fd9531bc6458a
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-22 12:36:00 +0430

    Numbers

--- Arrays/script.js
+++ Arrays/script.js
@@ -6,43 +6,43 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: "Jonas Schmedtmann",
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
   ],
-  currency: 'EUR',
-  locale: 'pt-PT', // de-DE
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: "Jessica Davis",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
   ],
-  currency: 'USD',
-  locale: 'en-US',
+  currency: "USD",
+  locale: "en-US",
 };
 
 const accounts = [account1, account2];
@@ -240,15 +240,27 @@ btnSort.addEventListener("click", (e) => {
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// LECTURES
+// LECTURE
 
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
+console.log(23 === 23.0);
+console.log(0.1 + 0.2);
+console.log(Number.parseInt("30px", 10)); // 30
+console.log(Number.parseInt("e23", 10)); // NaN
+console.log(Number.parseInt("  2.5rem  "));
+console.log(Number.parseFloat("   2.5rem"));
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// Just for cheking if a value is NaN
+console.log(Number.isNaN(20)); //false
+console.log(Number.isNaN("20")); //flase
+console.log(Number.isNaN(+"20X")); //true
+console.log(Number.isNaN(23 / 0)); //flase!!
 
-/////////////////////////////////////////////////
+// It's better to use this method ~> isFinite() to figurout something is a number or not
+console.log(Number.isFinite(20)); //true
+console.log(Number.isFinite("20")); //false
+console.log(Number.isFinite(+"20X")); //false
+console.log(Number.isFinite(23 / 0)); //false
 
+console.log(Number.isInteger(23));  //true
+console.log(Number.isInteger(23.0)); //true
+console.log(Number.isInteger(23 / 0)); //false
