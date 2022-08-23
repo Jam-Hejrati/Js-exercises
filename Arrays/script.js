b2a6de92e4b9bd8c6dba58fea24a01e951e5573c
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-23 09:02:53 +0430

    dates

--- Arrays/script.js
+++ Arrays/script.js
@@ -242,4 +242,36 @@ btnSort.addEventListener("click", (e) => {
 /////////////////////////////////////////////////
 // LECTURE
 
-//test
+// Create a date
+const now = new Date();
+console.log(now);
+
+console.log(new Date("Aug 23 2022 08:38:05"));
+console.log(new Date("December 24, 2015"));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 16, 15, 23, 5));
+console.log(new Date(2037, 10, 33));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+const future = new Date(2037, 10, 16, 15, 23)
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2141985180000));
+
+console.log(Date.now());
+
+future.setFullYear(2040)
+console.log(future);
