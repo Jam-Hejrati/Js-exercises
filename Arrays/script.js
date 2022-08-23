4fc472b2c04683dc7a8b24b099dc28b6a8fb5e7e
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-23 11:51:51 +0430

    times and dates fixed-2 --bankist

--- Arrays/script.js
+++ Arrays/script.js
@@ -16,10 +16,10 @@ const account1 = {
     "2019-12-23T07:42:02.383Z",
     "2020-01-28T09:15:04.904Z",
     "2020-04-01T10:17:24.185Z",
-    "2020-05-08T14:11:59.604Z",
-    "2020-05-27T17:01:17.194Z",
-    "2020-07-11T23:36:17.929Z",
-    "2020-07-12T10:51:36.790Z",
+    "2022-08-18T14:11:59.604Z",
+    "2022-08-20T17:01:17.194Z",
+    "2022-08-21T23:36:17.929Z",
+    "2022-08-22T10:51:36.790Z",
   ],
   currency: "EUR",
   locale: "pt-PT", // de-DE
@@ -72,6 +72,24 @@ const inputTransferAmount = document.querySelector(".form__input--amount");
 const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
+////////////////////////////////////////////
+// Functions
+
+const formatMovementDate = (date) => {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if (daysPassed === 0) return "Today";
+  if (daysPassed === 1) return "Yesterday";
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
 
 // set data visually in the movement box
 const displayMovements = function (acc, sort = false) {
@@ -85,10 +103,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const date = new Date(acc.movementsDates[index]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
             <div class="movements__row">
@@ -154,7 +169,9 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 }; // Just to make it globally
 
-// Event handler
+///////////////////////////////////////////////
+// Event handlers
+
 let currentAccount;
 btnLogin.addEventListener("click", (e) => {
   //Prevent form from submitting
@@ -208,8 +225,8 @@ btnTransfer.addEventListener("click", (e) => {
     receiverAcc.movements.push(amount);
 
     // Add Transfer Date
-    currentAccount.movementsDates.push(new Date().toISOString())
-    receiverAcc.movementsDates.push(new Date().toISOString())
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -228,7 +245,7 @@ btnLoan.addEventListener("click", (e) => {
     currentAccount.movements.push(amount);
 
     // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString())
+    currentAccount.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -267,3 +284,5 @@ btnSort.addEventListener("click", (e) => {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURE
+
+
