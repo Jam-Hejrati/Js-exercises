01c53eb7aebae12fe842a920958646638bb40fa9
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-29 17:58:47 +0430

    event delegation

--- Bankist website/script.js
+++ Bankist website/script.js
@@ -8,7 +8,6 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,6 +31,38 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+///////////////////////////////////////
+// smooth scroll Read more btn
+
+const section1 = document.querySelector("#section--1");
+
 btnScrollTo.addEventListener("click", function (e) {
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+/////////////////////////////////////////
+// Page navigation
+
+/*document.querySelectorAll(".nav__link").forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = this.getAttribute("href");
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  });
+});*/
+
+// Event delegation steps:
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+/////////////////////////////////////////
