644b8f176e35ed500f7afbf4c50ab547939dbbda
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-29 18:21:53 +0430

    DOM Traversing

--- Bankist website/script.js
+++ Bankist website/script.js
@@ -66,3 +66,29 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 });
 
 /////////////////////////////////////////
+// DOM Traversing
+const h1 = document.querySelector('h1')
+
+// going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'salmon'
+h1.lastElementChild.style.color = 'white'
+
+// going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+h1.closest('.header').style.background = 'var(--gradient-secondary)'
+h1.closest('h1').style.background = 'var(--gradient-primary)'
+
+// going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+console.log(h1.nextSibling);
+console.log(h1.previousSibling);
+console.log(h1.parentElement.children); //all siblings
+[...h1.parentElement.children].forEach(function(el){
+  if (el !== h1) el.style.transform = 'scale(0.5)'
+})
+
