8e51219d9807f9b5230dbc3cd30a91bfbead3dc1
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-31 16:22:43 +0430

    Intersection Observer API

--- Bankist website/script.js
+++ Bankist website/script.js
@@ -115,3 +115,21 @@ nav.addEventListener("mouseover", hoverHandler.bind(0.5));
 nav.addEventListener("mouseout", hoverHandler.bind(1));
 
 ////////////////////////////////////////////
+// sticky navigation: Intersection Observer API
+
+const header = document.querySelector('.header')
+const navHeight = nav.getBoundingClientRect().height
+
+const stickyNav = function(entries){
+  const [entry] = entries;
+  console.log(entry);
+  if(!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+}
+
+const headerObserver = new IntersectionObserver(stickyNav , {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+})
+headerObserver.observe(header)
