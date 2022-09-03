8d31768aff3d94c1a8c479004aa3debf8b80b15c
Jam Hejrati <jamhejratipro@gmail.com>
2022-09-03 11:31:22 +0430

    constructor make object

--- OOP/script.js
+++ OOP/script.js
@@ -1,2 +1,22 @@
 'use strict'
 
+const Person = function(firstName , birthYear){
+    // Instance properties
+    this.firstName = firstName
+    this.birthYear = birthYear
+
+    // Bad practice for make method in constructor function
+    /*this.calcAge = function(){
+        console.log(2022 - this.birthYear);
+    }*/
+}
+
+const jam = new Person('Jam' , 2000)
+const jonas = new Person('Jonas' , 1991)
+const jack = new Person('Jack' , 1975)
+
+console.table(jam);
+console.table(jonas);
+console.table(jack);
+console.log(jam instanceof Person);
+
