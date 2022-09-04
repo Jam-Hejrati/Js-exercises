eca7845f30a8fff3c83809af622f8cace32de500
Jam Hejrati <jamhejratipro@gmail.com>
2022-09-04 05:19:48 +0430

    Getter & Setter

--- OOP/script.js
+++ OOP/script.js
@@ -1,20 +1,55 @@
 "use strict";
 
-// Class Expression
-// const PersonCl = class {}
-
-// Class Decleration
 class PersonCl {
-    constructor(firstName , birthYear){
-        this.firstName = firstName
-        this.birthYear = birthYear
-    }
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
 
-    calcAge(){
-        console.log(2022 - this.birthYear);
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2022 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(" ")) {
+      this._fullName = name;
+    } else {
+      alert(`${name} is not a full name!`);
     }
+  }
+
+  get fullName(){
+    return this._fullName
+  }
 }
 
-const Jam = new PersonCl('Jam' , 2000)
-console.log(Jam);
-Jam.calcAge()
+const jam = new PersonCl("Jam Hejrati", 2000);
+console.log(jam.age);
+console.log(jam);
+
+
+///////////////////////
+/*const account = {
+  owner: "Jam",
+  movements: [200, 530, 120, 300],
+
+  get latest(){
+    return this.movements.slice(-1).pop()
+  },
+
+  set latest(mov){
+    this.movements.push(mov)
+  }
+};
+
+console.log(account.latest);
+account.latest = 50
+console.log(account.movements);*/
