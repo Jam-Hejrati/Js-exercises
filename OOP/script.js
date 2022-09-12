1d0e2be171de11c97d09d5ae3d938e51471431af
Jam Hejrati <jamhejratipro@gmail.com>
2022-09-12 10:51:23 +0430

    Encapsulation Private Fields

--- OOP/script.js
+++ OOP/script.js
@@ -1,53 +1,60 @@
 "use strict";
 
+// Public fields
+// Private fields
+// Public methods
+// Private methods
+
 class Account {
+  // Public fields (instances)
+  locale = navigator.language;
+
+  // Private fields
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    
-    // Prtected Property
-    this._pin = pin;
-    this._movements = [];
-    
-    this.locale = navigator.language;
-
+    this.#pin = pin;
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
-  getMovements(){
-    return this._movements
+  // Public methods
+  getMovements() {
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val){
-    return true;
-  }
-
-  requestLoan(val){
-    if(this.approveLoan(val)){
-      this.deposit(val)
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
       console.log(`Loan approved`);
     }
+  }
+  
+  // Private methods
+  _approveLoan(val) {
+    return true;
   }
 }
 
 const acc1 = new Account("Jam", "IR", 1111);
 console.log(acc1);
 
-// acc1._movements.push(250);
-// acc1._movements.push(-140);
-acc1.deposit(250)
-acc1.withdraw(140)
-acc1.requestLoan(1000)
-acc1.approveLoan(1000)
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
 console.log(acc1.getMovements());
-
 console.log(acc1);
-console.log(acc1.pin);
+
+// acc1.#approveLoan(1000); Err) Private field
+// console.log(acc1.#pin);  Err) Private field
+// console.log(acc1.#movements);  Err) Private field
