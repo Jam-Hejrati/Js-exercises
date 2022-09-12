5565957539e7d2de9595741d5ed867823ffd6b7d
Jam Hejrati <jamhejratipro@gmail.com>
2022-09-12 10:30:09 +0430

    Encapsulation _

--- OOP/script.js
+++ OOP/script.js
@@ -4,27 +4,35 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    
+    // Prtected Property
+    this._pin = pin;
+    this._movements = [];
+    
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
+
+  getMovements(){
+    return this._movements
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val){
+  _approveLoan(val){
     return true;
   }
 
   requestLoan(val){
     if(this.approveLoan(val)){
-      this.deposite(val)
+      this.deposit(val)
       console.log(`Loan approved`);
     }
   }
@@ -33,11 +41,13 @@ class Account {
 const acc1 = new Account("Jam", "IR", 1111);
 console.log(acc1);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 acc1.deposit(250)
 acc1.withdraw(140)
 acc1.requestLoan(1000)
 acc1.approveLoan(1000)
-console.log(acc1);
+console.log(acc1.getMovements());
 
+console.log(acc1);
+console.log(acc1.pin);
