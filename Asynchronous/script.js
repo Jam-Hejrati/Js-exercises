7f7f6c697236d9a5d9b7d8bae598d334803867b4
Jam Hejrati <jamhejratipro@gmail.com>
2022-09-14 15:55:36 +0430

    XMLHttpRequest()

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -1,6 +1,39 @@
-'use strict'
+"use strict";
 
-const btn = document.querySelector('.btn-country')
-const countriesContainer = document.querySelector('.countries')
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+    <article class="country">
+          <img class="country__img" src="${data.flags["png"]}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name["common"]}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1_000_000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages.fas}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies.IRR.name
+            }</p>
+          </div>
+        </article>`;
+
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+
+getCountryData('Iran')
