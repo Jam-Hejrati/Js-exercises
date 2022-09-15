d41eff55c3c9ec8bf53b527600f14d0251a461c0
Jam Hejrati <jamhejratipro@gmail.com>
2022-09-15 11:58:39 +0430

    Callback Hell

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////
-
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
@@ -36,4 +36,58 @@ const getCountryData = function (country) {
 };
 
 
-getCountryData('Iran')
+getCountryData('Iran')*/
+////////////////////////////////////////////
+
+const renderCountry = function (data , className ='') {
+  const html = `
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flags["png"]}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name["common"]}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1_000_000
+            ).toFixed(1)} people</p>
+          </div>
+        </article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render Country 1
+    renderCountry(data);
+
+    // Get neighbour Country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call neighbour countries
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2 , 'neighbour')
+    });
+  });
+};
+
+// getCountryAndNeighbour("Iran");
+// getCountryAndNeighbour('portugal')
+getCountryAndNeighbour('usa')
