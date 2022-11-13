c5c2924c533b74e8c200098b098a3ea7cbf394b2
Jam <jamhejratipro@gmail.com>
2022-11-13 14:39:53 +0330

    a bug fix

--- Asynchronous-FC/script.js
+++ Asynchronous-FC/script.js
@@ -26,7 +26,7 @@ myPromise.then(function(data){
 // async / await
 async function sendRequest() {
     try {
-        const response = await fetch("https://jsonplaceholder.typicode/todos/1")
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
         console.table(response)
         const data = await response.json()
         console.log(data)
