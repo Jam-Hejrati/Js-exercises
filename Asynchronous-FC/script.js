08ba1482bd98276ef57376bf2680b249143b5a60
Jam <jamhejratipro@gmail.com>
2022-11-13 13:03:19 +0330

    learn: promises error handling without .catch()

--- Asynchronous-FC/script.js
+++ Asynchronous-FC/script.js
@@ -1,14 +1,19 @@
 "use strict"
 
 // Promises
-/*fetch("https://jsonplaceholder.typicode.com/todos/1").then(function (response) {
-    return response.json()
-}).then(function (data) {
-    console.table(data)
-})*/
+fetch("https://jsonplaceholder.typicode/todos/1")
+    .then(function (response) {
+            return response.json()
+        },
+        function (err) {
+            console.log("Error!")
+        })
+    .then(function (data) {
+        console.table(data)
+    })
 
 // Custom Promises
-const myPromise = new Promise((resolve , reject ) => {
+/*const myPromise = new Promise((resolve , reject ) => {
     // pending - resolve - reject
     setTimeout(function(){
         resolve('It woreked.')
@@ -16,4 +21,4 @@ const myPromise = new Promise((resolve , reject ) => {
 })
 myPromise.then(function(data){
     console.log(data)
-})
+})*/
