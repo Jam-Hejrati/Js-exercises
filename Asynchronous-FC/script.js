176b12c5b23498b4fe9ff400e85c5ce08e8801a3
Jam <jamhejratipro@gmail.com>
2022-11-13 12:56:34 +0330

    learn: custom promises

--- Asynchronous-FC/script.js
+++ Asynchronous-FC/script.js
@@ -1,8 +1,19 @@
 "use strict"
 
 // Promises
-fetch("https://jsonplaceholder.typicode.com/todos/1").then(function (response) {
+/*fetch("https://jsonplaceholder.typicode.com/todos/1").then(function (response) {
     return response.json()
 }).then(function (data) {
     console.table(data)
+})*/
+
+// Custom Promises
+const myPromise = new Promise((resolve , reject ) => {
+    // pending - resolve - reject
+    setTimeout(function(){
+        resolve('It woreked.')
+    } , 3000)
+})
+myPromise.then(function(data){
+    console.log(data)
 })
