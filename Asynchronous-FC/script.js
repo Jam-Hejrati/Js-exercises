d9623530b97de44ea8eabc0424a017537693b864
Jam <jamhejratipro@gmail.com>
2022-11-13 13:55:02 +0330

    learn: async / await

--- Asynchronous-FC/script.js
+++ Asynchronous-FC/script.js
@@ -1,7 +1,7 @@
 "use strict"
 
 // Promises
-fetch("https://jsonplaceholder.typicode/todos/1")
+/*fetch("https://jsonplaceholder.typicode.com/todos/1")
     .then(function (response) {
         return response.json()
     })
@@ -11,14 +11,28 @@ fetch("https://jsonplaceholder.typicode/todos/1")
     .then(function (data) {
         console.table(data)
     })
-
+*/
 // Custom Promises
 /*const myPromise = new Promise((resolve , reject ) => {
     // pending - resolve - reject
     setTimeout(function(){
-        resolve('It woreked.')
+        resolve('It worked.')
     } , 3000)
 })
 myPromise.then(function(data){
     console.log(data)
 })*/
+
+// async / await
+async function sendRequest() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode/todos/1")
+        console.table(response)
+        const data = await response.json()
+        console.log(data)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+sendRequest()
