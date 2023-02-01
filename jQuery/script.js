572279ca222d51e9d688df5602524b191ceccfd3
Jam <jamhejratipro@gmail.com>
2023-02-01 12:46:22 +0330

    fix: fix jQuery deprecations

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,15 +1,15 @@
-$(document).ready(function () {
+$(function () {
   $(".text").text("Hello World!");
 
-  $(".buttons button:nth-child(1)").click(function () {
+  $(".buttons button:nth-child(1)").on('click' , function () {
     $(".text").hide();
   });
 
-  $(".buttons button:nth-child(2)").click(function () {
+  $(".buttons button:nth-child(2)").on('click', function () {
     $(".text").show();
   });
 
-  $(".buttons button:nth-child(3)").click(function () {
+  $(".buttons button:nth-child(3)").on('click',function () {
     $(".text").toggle(500); // 500 milliseconds
   });
 
@@ -17,55 +17,55 @@ $(document).ready(function () {
     console.log(index + 1 + ": " + $(this).text());
   });
 
-  $("button").attr("title", "click me");
+  $("button").attr("title" , "click me");
 
   $("p").removeAttr("title");
 
-  $(".second-section input").focus(function () {
-    $(this).css("background-color", "#1098ad");
-    $(this).mouseleave(function () {
-      $(this).css("background-color", "transparent");
+  $(".second-section input").on('focus',function () {
+    $(this).css("background-color" , "#1098ad");
+    $(this).on('mouseleave',function () {
+      $(this).css("background-color" , "transparent");
     });
   });
 
   $(".third-section input").on({
     mouseenter: function () {
-      $(this).css("background-color", "aqua");
-    },
+      $(this).css("background-color" , "aqua");
+    } ,
     mouseleave: function () {
-      $(this).css("background-color", "lightgray");
-    },
+      $(this).css("background-color" , "lightgray");
+    } ,
     click: function () {
-      $(this).css("background-color", "red");
-    },
+      $(this).css("background-color" , "red");
+    } ,
   });
 
-  $(".btn-fadein").click(function () {
+  $(".btn-fadein").on('click',function () {
     $(".text-section").fadeIn();
   });
-  $(".btn-fadeout").click(function () {
+  $(".btn-fadeout").on('click',function () {
     $(".text-section").fadeOut("slow");
   });
-  $(".btn-fadetoggle").click(function () {
-    $(".text-section").fadeToggle(1000); //milisecond
+  $(".btn-fadetoggle").on('click',function () {
+    $(".text-section").fadeToggle(1000); //millisecond
   });
 
-  $(".btn-slideup").click(function () {
+  $(".btn-slideup").on('click',function () {
     $(".slide-text-section").slideUp();
   });
-  $(".btn-slidedown").click(function () {
+  $(".btn-slidedown").on('click',function () {
     $(".slide-text-section").slideDown("slow");
   });
-  $(".btn-slidetoggle").click(function () {
-    $(".slide-text-section").slideToggle(1000); //milisecond
+  $(".btn-slidetoggle").on('click',function () {
+    $(".slide-text-section").slideToggle(1000); //millisecond
   });
 
-  $(".animate-btn").click(function () {
+  $(".animate-btn").on('click',function () {
     $('.animate-section > span').animate({
-      width: '+=150px',
-      fontSize: '2rem',
+      width: '+=150px' ,
+      fontSize: '2rem' ,
       left: '350px'
     })
-    $('.animate-section > span').animate({ width: 'toggle' } , 'fast')
+    $('.animate-section > span').animate({width: 'toggle'} , 'fast')
   });
 }); // end of jq
