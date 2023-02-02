baff3872b17d3ca5d0136b51bd0493e84b521d21
Jam <jamhejratipro@gmail.com>
2023-02-02 23:57:32 +0330

    html() & an element argument

--- jQuery/script.js
+++ jQuery/script.js
@@ -5,11 +5,11 @@ $(function () {
     $(".text").hide();
   });
 
-  $(".buttons button:nth-child(2)").on('click', function () {
+  $(".buttons button:nth-child(2)").on('click' , function () {
     $(".text").show();
   });
 
-  $(".buttons button:nth-child(3)").on('click',function () {
+  $(".buttons button:nth-child(3)").on('click' , function () {
     $(".text").toggle(500); // 500 milliseconds
   });
 
@@ -21,9 +21,9 @@ $(function () {
 
   $("p").removeAttr("title");
 
-  $(".second-section input").on('focus',function () {
+  $(".second-section input").on('focus' , function () {
     $(this).css("background-color" , "#1098ad");
-    $(this).on('mouseleave',function () {
+    $(this).on('mouseleave' , function () {
       $(this).css("background-color" , "transparent");
     });
   });
@@ -40,32 +40,37 @@ $(function () {
     } ,
   });
 
-  $(".btn-fadein").on('click',function () {
+  $(".btn-fadein").on('click' , function () {
     $(".text-section").fadeIn();
   });
-  $(".btn-fadeout").on('click',function () {
+  $(".btn-fadeout").on('click' , function () {
     $(".text-section").fadeOut("slow");
   });
-  $(".btn-fadetoggle").on('click',function () {
+  $(".btn-fadetoggle").on('click' , function () {
     $(".text-section").fadeToggle(1000); //millisecond
   });
 
-  $(".btn-slideup").on('click',function () {
+  $(".btn-slideup").on('click' , function () {
     $(".slide-text-section").slideUp();
   });
-  $(".btn-slidedown").on('click',function () {
+  $(".btn-slidedown").on('click' , function () {
     $(".slide-text-section").slideDown("slow");
   });
-  $(".btn-slidetoggle").on('click',function () {
+  $(".btn-slidetoggle").on('click' , function () {
     $(".slide-text-section").slideToggle(1000); //millisecond
   });
 
-  $(".animate-btn").on('click',function () {
+  $(".animate-btn").on('click' , function () {
     $('.animate-section > span').animate({
       width: '+=150px' ,
       fontSize: '2rem' ,
       left: '350px'
     })
-    $('.animate-section > span').animate({width: 'toggle'} , 'fast')
+    $(".animate-section > span").animate({width: 'toggle'} , 'fast')
   });
+
+  $(".btn-transform").on('click' , () => {
+    $(".html-transform").html("<span class='badge bg-secondary'>transformed</span>")
+  })
+
 }); // end of jq
